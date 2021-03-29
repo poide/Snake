072c810775b4b19709c13738d22c9c631e3b4275
poide <poide@pop-os.localdomain>
2021-03-29 16:40:50 +0200

    Adding an image for the score panel, creating a module for the graphic code, and now the snake goes faster when she eats

--- snake.py
+++ snake.py
@@ -1,33 +1,10 @@
-from game import Game
-import time
 import pygame
-from pygame.locals import *
+import graphics
+from game import Game
 
-def renderFrame(game,window):
-    window.fill((128,128,128)) #Fill the screen with light gray,  the background
-    i = 0
-    j = 0
-    currentBoard = game.board  #From the game we get the board and we travel it, painting the board
-    while i < 100:
-        if i%10==0 and i != 0:
-            j = j + 1
-        posx = 25 * (i%10)
-        posy = 25 * j
-        if currentBoard[i] == 0 : #If the position contains a 0 it means there's nothing in it, so we paint it with a black square
-            pygame.draw.rect(window, (0,0,0), pygame.Rect(posx, posy, 20, 20)) 
-        elif currentBoard[i] == -1: #If the position contains a -1 that position contains the food, so we paint it with a red square
-            pygame.draw.rect(window, (255,0,0), pygame.Rect(posx, posy, 20, 20))
-        elif currentBoard[i] == 1:  #Finally, if the position contains an 1, it means the position contains a part of the snake, so we paint it green
-            pygame.draw.rect(window, (0,255,0), pygame.Rect(posx, posy, 20, 20))
-        pygame.font.init()
-        i = i +1
 
-    myfont = pygame.font.SysFont('Comic Sans MS', 30)                             #
-    ScoreTextSurface = myfont.render('Score', False, (0, 0, 0))                   #
-    window.blit(ScoreTextSurface,(260,5))                                         #  In this five lines I have the code to render the "Score text" and the
-    NumberScoreSurface = myfont.render(str(game.score), False, (0, 0, 0))         #                          score number
-    window.blit(NumberScoreSurface,(275,30))                                      #
-    pygame.display.flip()                                                         # In this line I update the frame
+
+
 
 def pauseMenu():    
     check = True
@@ -42,33 +19,23 @@ def pauseMenu():
                     check = False
     return
 
-
-
-
-
-
-
 def main():
     pygame.init()                                               
-    window = pygame.display.set_mode((500, 500))            
+    window = pygame.display.set_mode((350, 245))            
     pygame.display.flip()
     pygame.display.set_caption("Snake by PepAss'o")
-
-    
-    
     game = Game()
     fixInput = True
     run = True
     while run:
-
-        pygame.time.delay(50)
+        x = 75
+        if(game.score % 5 == 0):
+            x = x - 5
+        pygame.time.delay(x)
         if game.state == "Win":
             game = Game()
         elif game.state == "Dead":
             print("Has perdido")
-            print(game.boardToConsole())
-            print(game.facing)
-            print(game.positionList)
             game = Game()
         events = pygame.event.get()    
         for event in events:
@@ -89,8 +56,7 @@ def main():
                         run = False
         if fixInput == True:
             game.move()
-            renderFrame(game,window)
-
+            graphics.renderFrame(game,window)
         fixInput = not fixInput
     pygame.quit()
 if __name__ == "__main__":
